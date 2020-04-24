e2c4c3a1b3c47ab04e24b28d449fccbb3b61b8bc
矢野大樹 <daikitech0123@gmail.com>
2020-04-24 16:32:02 +0900

    add it

---
+++ front/pages/signup.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <h1>会員登録</h1>
+        <b-field label="苗字">
+            <b-input v-model="first_name"></b-input>
+        </b-field>
+        <b-field label="Email"
+            type="is-danger"
+            message="適切なアドレスを入力してください">
+            <b-input type="email"
+                maxlength="30" v-model="email">
+            </b-input>
+        </b-field>
+        <b-field label="Password">
+            <b-input type="password"
+                password-reveal>
+            </b-input>
+        </b-field>
+    </section>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            first_name : "",
+            email : "",
+            password : ""
+        }
+    }
+
+}
+</script>
+
