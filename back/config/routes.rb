e2f155c37e97851d84b223ea5e9796b0df21c230
矢野大樹 <daikitech0123@gmail.com>
2020-05-11 19:40:15 +0900

    サーバーサイド　楽天api導入

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       #Sign Up用のみ
+      get "/search", to: "books#search"
       resource :users, only: [:create]
       post "/create", to: "auth#create"
       post "/signin", to: "auth#signin"
