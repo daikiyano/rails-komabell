75f03cd69a9933b1a6d348f51420fdf23e1db724
矢野大樹 <daikitech0123@gmail.com>
2020-05-27 18:37:00 +0900

    no message

---
+++ front/pages/users/MySkill.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+
+    <h1>My Edit</h1>
+        <MyPageTab :activeTab="activeTab" />
+   
+   <!-- {{user.email}} -->
+   <!-- <component :is="MyPageComponent" :user="this.user"></component> -->
+   
+</div>
+</template>
+<script>
+import MyPageTab from '~/components/users/MyPageTab.vue'
+
+export default {
+    components: {
+        MyPageTab
+    },
+    data() {
+    return {
+      activeTab: 2
+    }
+    
+  },
+
+   methods: {
+
+   }
+
+}
+
+</script>
